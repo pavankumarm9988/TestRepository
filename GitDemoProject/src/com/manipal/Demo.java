b6f809507b914c8bf11a2cdbc68dc54f784da30a
ITFS <ITFS@ITFS-17>
2019-09-05 11:32:59 +0530

    updated

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello GIT");
 		System.out.println("second kine is added");
+		System.out.println("new line");
 
 	}
 
